97ee49ba808844fd8fff9f008639e51942d71e9c
jesjonesc <58461507+yrshva@users.noreply.github.com>
2022-09-14 23:01:45 +0300

    added api key as enviromental variable

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,22 @@ module.exports = {
         test: ["react-toastify", "exceljs", "file-saver"],
       },
     ],
+    [
+      "module:react-native-dotenv",
+      {
+        envName: "APP_ENV",
+        moduleName: "@env",
+        path: ".env",
+        blocklist: null,
+        allowlist: null,
+        blacklist: null, // DEPRECATED
+        whitelist: null, // DEPRECATED
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+
     "react-native-reanimated/plugin",
   ],
 };
