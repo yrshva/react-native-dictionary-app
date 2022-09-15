436fa3c1cc247c5e98c4da271a4b68e8ddf593dd
jesjonesc <58461507+yrshva@users.noreply.github.com>
2022-09-15 03:17:53 +0300

    created about page

--- screens/about.tsx
+++ screens/about.tsx
@@ -1,6 +1,20 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export const About = () => {
-  return <Text>About</Text>;
+  return (
+    <View style={styles.container}>
+      <Text>
+        Dictionary application was developed by Anastasiia Yershova to search
+        for words' definitions and see examples of using it.
+      </Text>
+    </View>
+  );
 };
+const styles = StyleSheet.create({
+  container: {
+    width: "90%",
+    alignSelf: "center",
+    marginTop: 30,
+  },
+});
