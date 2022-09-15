e2fe2b7320b3abc184e1c6913d7581c85942de34
jesjonesc <58461507+yrshva@users.noreply.github.com>
2022-09-15 03:18:57 +0300

    added styles

--- screens/HomeComponents/Definitions.tsx
+++ screens/HomeComponents/Definitions.tsx
@@ -6,18 +6,32 @@ import { uid } from "uid";
 interface Props {
   definitions: { definition: string; example: string }[];
 }
-const Definitions = (props: Props) => {
-  return (
-    <View>
-      {props.definitions.map((definition, index) => (
-        <View key={uid()}>
-          <Text>
-            {index + 1}. {definition.definition}
-          </Text>
-          {definition.example && <Text>e.g. {definition.example}</Text>}
-        </View>
-      ))}
-    </View>
-  );
-};
+const Definitions = (props: Props) => (
+  <>
+    {props.definitions.map((definition, index) => (
+      <View key={uid()} style={styles.definitionWrapper}>
+        <Text style={styles.definition}>
+          {index + 1}. {definition.definition}
+        </Text>
+        {definition.example && (
+          <Text style={styles.example}>e.g. {definition.example}</Text>
+        )}
+      </View>
+    ))}
+  </>
+);
 export default Definitions;
+
+const styles = StyleSheet.create({
+  definitionWrapper: {
+    marginVertical: 2,
+  },
+  definition: {
+    fontFamily: "Inter_300Light",
+  },
+  example: {
+    marginVertical: 2,
+    fontSize: 12,
+    fontFamily: "Inter_200ExtraLight",
+  },
+});
