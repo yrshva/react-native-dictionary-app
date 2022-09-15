0734b999644880aac6e8c27591bf607f8ed8d8a7
jesjonesc <58461507+yrshva@users.noreply.github.com>
2022-09-15 03:18:34 +0300

    added cutom fonts

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,28 +1,47 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { ScrollView, Text, StyleSheet } from "react-native";
-import Dictionary from "./HomeComponents/Dictionary";
+import { useState } from "react";
+import { ScrollView, Text, StyleSheet, ActivityIndicator } from "react-native";
+import {
+  useFonts,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
 import { SearchForm } from "./HomeComponents/SearchForm";
+import Dictionary from "./HomeComponents/Dictionary";
 
 export const Home = () => {
   const [keyword, setKeyword] = useState<String | null>(null);
+  const [fontsLoaded] = useFonts({
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_500Medium,
+  });
   const handleSubmit = (value: String) => {
     setKeyword(value);
   };
-  return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.header}>What word would you like to look up?</Text>
-      <SearchForm handleSubmit={handleSubmit} />
-      {keyword != null && <Dictionary keyword={keyword} />}
-    </ScrollView>
-  );
+  if (fontsLoaded) {
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={[styles.header]}>
+          What word would you like to look up?
+        </Text>
+        <SearchForm handleSubmit={handleSubmit} />
+        {keyword != null && <Dictionary keyword={keyword} />}
+      </ScrollView>
+    );
+  } else return <ActivityIndicator size="large" color="#BEDBDB" />;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 15,
+    backgroundColor: "#ECF8F8",
   },
   header: {
     alignSelf: "center",
+    fontFamily: "Inter_500Medium",
+    fontSize: 18,
   },
 });
